11d38f562e3c929302e966384ad0b7b47a74d41c
Ian <ianpc122@gmail.com>
2020-02-18 18:25:30 +0000

    mvp complete

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      object:{},
+      selectedCurrency:0,
+      amountToExchange:0
+    },
+    mounted(){
+      this.getcurrency()
+    },
+    computed: {
+      exchange: function() {
+        return (this.selectedCurrency / this.amountToExchange).toFixed(2);
+      }
+    },
+    methods:{
+      getcurrency: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(object => this.object = object)
+      }
+
+
+    }
+  }
+)
+});
